27cc605a39eb0bff887dcdee3e43fe1dab0647c6
czar-zorengg <agrazadaczarkristoffer@gmail.com>
2026-08-13 14:06:41 +0800

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://cuhdvpgqbjzkmrinbyjh.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_NI2lEYRyMo6u8tZMF3RHNA_so3UxL30";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
